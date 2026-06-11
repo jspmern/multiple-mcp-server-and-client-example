9f6bf51d711aeca63d5208fd1d1f1a1e91151cee
jspmern <jspmern@gmail.com>
2026-06-12 00:37:02 +0530

    add  tool for mcp

--- hr-mcp-server/index.ts
+++ hr-mcp-server/index.ts
@@ -1,1 +1,54 @@
 // placeholder: hr-mcp-server/index.ts
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+import { employees } from "./data/employees.ts";
+
+/** this is the server instance */
+const server = new McpServer({
+  name: "'hr-mcp-server",
+  version: "1.0.0",
+});
+
+/**Adding the tool here later i can move to seprate file */
+server.registerTool(
+  "get_all_emp_data",
+  {
+    description: "this tool is getting all the employees data",
+    inputSchema: {
+      limit: z
+        .string()
+        .describe("this is will show how many employee data will return"),
+    },
+  },
+  async ({limit}) => {
+    const count =
+      typeof limit === "string"
+        ? parseInt(limit, 10) || 2
+        : typeof limit === "number"
+        ? limit
+        : 2;
+    const data = employees.slice(0, count);
+
+    return {
+      content: [
+        {
+          type: "text",
+          text: JSON.stringify(data),
+        },
+      ],
+    };
+  },
+);
+
+/** code for running mcp in local for testing right now check in claude but later we will do by our own client(local) */
+async function main() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+  console.error("Weather MCP Server running on stdio");
+}
+
+main().catch((error) => {
+  console.error("Fatal error in main():", error);
+  process.exit(1);
+});
